fa7e8af7b9d9b1d49d4cccd7f9ccb6d3155ab695
Leo Lu <ll865@cornell.edu>
2025-01-27 20:01:34 -0500

    Added miku

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 import * as THREE from 'three';
 import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
 
 const scene = new THREE.Scene();
+const pivot = new THREE.Group();
+scene.add(pivot)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-//document.body.appendChild(renderer.domElement);
+const loader = new GLTFLoader();
+let miku;
+
+loader.load('scene.gltf', function (gltf) {
+
+  miku = gltf.scene;
+  pivot.add(miku)
+  miku.position.set(-150, -200, 100);
+
+}, undefined, function (error) {
+
+  console.error(error);
+
+});
 
 const pointLight1 = new THREE.PointLight(0xffffff, 3, 0, 0);
 pointLight1.position.set(500, 500, 500);
@@ -21,8 +36,14 @@ const material = new THREE.MeshPhongMaterial({ flatShading: true })
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 3;
+camera.position.z = 200;
+camera.position.y = -50;
 
+if (miku) miku.translateY(-500000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 
 const effect = new AsciiEffect(renderer, ' .:-+*=%@#', { invert: true });
@@ -31,10 +52,16 @@ effect.domElement.style.color = 'white';
 effect.domElement.style.backgroundColor = 'black';
 
 document.body.appendChild(effect.domElement);
+const controls = new TrackballControls(camera, effect.domElement);
+const axis = new THREE.Vector3(0, 1, 0);
+let rad = 0.01;
 
 function animate() {
-  //renderer.render(scene, camera);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  pivot.rotateOnAxis(axis, rad);
+
+  controls.update();
   effect.render(scene, camera);
 }
